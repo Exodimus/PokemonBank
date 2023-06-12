f4bc27d21307720f154547d20ffcdf93abcf8732
Exodimus <jorgemixcruz@gmail.com>
2023-06-11 23:08:42 -0600

    Implementación de local storage

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 // Obtiene los items del nav
 const navItems = document.querySelectorAll('.nav-item');
 const lblAct = document.querySelector('.lblAct');
-const div = document.querySelector('.mb-3');
+const div = document.querySelector('.dashboard');
 const formLogin = document.querySelector('.login');
 const pin = document.querySelector('#pin');
-//const transacList = document.querySelector('.transacList');
+const btnTransac = document.querySelector('#enviar');
+const transacList = document.querySelector('.transacList')
+
+let saldo = localStorage.getItem("saldo") || 500;
+let historial = JSON.parse(localStorage.getItem("historial")) || [];
 
 //Valida el login
 if (formLogin != null) {
@@ -29,12 +33,6 @@ if (formLogin != null) {
   }
 }
 
-  /**
-   * 
-   *    ;
-   */
-
-
 // Recorre todos los items
 navItems.forEach(navItem => {
   navItem.addEventListener('click', () => {
@@ -46,21 +44,24 @@ navItems.forEach(navItem => {
     // Verifica el elemento seleccionado
     switch (navItem.querySelector('.nav-link').textContent) {
         case 'Depósito':
+          console.log(btnTransac);
+          btnTransac.classList.add("depositar")
             div.innerHTML = `<label for="formGroupExampleInput2" required class="form-label fw-bold lblAct">Nota</label>
-            <input type="text" min="1" class="form-control" id="formGroupExampleInput2">
+            <input type="text" min="1" class="form-control" id="nota">
             <label for="formGroupExampleInput2" required class="form-label fw-bold lblAct">Monto a depositar</label>
-            <input type="number" min="1" class="form-control" id="formGroupExampleInput2">
-            <button type="submit" class="btn btn-primary mt-3 bg-success">Enter</button>`;  
+            <input type="number" min="1" class="form-control" id="monto">
+            <button type="submit" id="enviar" onclick="transactControl()" class="btn btn-primary  mt-3 bg-success">Enter</button>`;  
           break;
         case 'Retiro':
-            div.innerHTML = `<label for="formGroupExampleInput2" required class="form-label fw-bold lblAct">Nota</label>
-            <input type="text" min="1" class="form-control" id="formGroupExampleInput2">
-            <label for="formGroupExampleInput2" required class="form-label fw-bold lblAct">Monto a retirar</label>
-            <input type="number" min="1" class="form-control" id="formGroupExampleInput2">
-            <button type="submit" class="btn btn-primary mt-3 bg-success">Enter</button>`;
+          btnTransac.classList.remove("depositar")
+            div.innerHTML = `<label for="nota" required class="form-label fw-bold lblAct">Nota</label>
+            <input type="text" min="1" class="form-control" id="nota">
+            <label for="monto" required class="form-label fw-bold lblAct">Monto a retirar</label>
+            <input type="number" min="1" class="form-control" id="monto">
+            <button type="submit" id="enviar" onclick="transactControl()"  class="btn btn-primary retirar mt-3 bg-success">Enter</button>`;
           break;
         case 'Consultar saldo':
-            div.innerHTML = `<h1 class="text-center">El saldo disponible es: $100</h1>`;            
+            div.innerHTML = `<h1 class="text-center">El saldo disponible es: $${localStorage.getItem("saldo")}</h1>`;            
           break;
         case 'Pago de servicios':
             div.innerHTML = `<ul class="list-group">
@@ -81,11 +82,124 @@ navItems.forEach(navItem => {
               <span class="badge bg-primary rounded-pill p-2 pagar">Pagar</span>
             </li>
           </ul>`;
-            
           break;
-        default:
-          break;
+
       } 
   });
 });
+  /**
+   * 
+   *    ;
+   */
+
+  function transactControl() {
+    let monto = parseInt(document.querySelector("#monto").value);
+    let nota = document.querySelector("#nota").value;
+    if (monto != '' && nota != '') {
+      if (btnTransac.classList.contains("depositar")) {
+        saldo += monto;
+        historial.push({ tipo: "deposito", monto: monto, nota: nota });
+        //Guarda los datos en local storage
+        localStorage.setItem("saldo", saldo);
+        localStorage.setItem("historial", JSON.stringify(historial));
+        //Limpia los campos
+        document.getElementById("monto").value = "";
+        document.getElementById("nota").value = "";
+      } else {
+        if (monto <= saldo) {
+          saldo -= monto;
+          historial.push({ tipo: "retiro", monto: monto, nota: nota });
+          //guarda los datos en local storage
+          localStorage.setItem("saldo", saldo);
+          localStorage.setItem("historial", JSON.stringify(historial));
+          //Limpia los campos
+          document.getElementById("monto").value = "";
+          document.getElementById("nota").value = "";
+        } else {
+          console.log("Saldo insuficiente");
+        }
+      }
+    } else {
+      console.log("No se permiten campos vacíos");
+    }
+  }
+  
+
+
+//Función que limpia los campos
+function limpiarCampos() {
+  document.getElementById("monto").value = "";
+  document.getElementById("nota").value = "";
+}
+
+window.addEventListener("load", function() {
+  if (window.location.href.includes("transacciones.html")) {
+    actualizarHistorial();
+    crearGrafica()
+  }
+});
+
+  //Actualiza el historial de transacciones dinámicamente
+  function actualizarHistorial() {
+    if(transacList != null){
+      transacList.innerHTML = "";
+      historial.forEach(function(transaccion) {
+        var li = document.createElement("li");
+        li.classList.add("list-group-item", "d-flex" ,"justify-content-between" ,"align-items-center")
+        if (transaccion.tipo == "deposito")
+          li.innerHTML = `${transaccion.nota} <span class="badge bg-danger rounded-pill p-2">+$${transaccion.monto}</span>`
+          else
+          li.innerHTML = `${transaccion.nota} <span class="badge bg-primary rounded-pill p-2">-$${transaccion.monto}</span>`
+        transacList.appendChild(li);
+      });
+    }
+}
+
+function obtenerCantidadDepositos() {
+  const historialLocalStorage = JSON.parse(localStorage.getItem('historial')) || [];
+  return historialLocalStorage.reduce(function (contador, transaccion) {
+    if (transaccion.tipo === 'deposito') {
+      return contador + 1;
+    } else {
+      return contador;
+    }
+  }, 0);
+}
+
+function obtenerCantidadRetiros() {
+  const historialLocalStorage = JSON.parse(localStorage.getItem('historial')) || [];
+  return historialLocalStorage.reduce(function (contador, transaccion) {
+    if (transaccion.tipo === 'retiro') {
+      return contador + 1;
+    } else {
+      return contador;
+    }
+  }, 0);
+}
+
+function crearGrafica(){
+    //Configuración para gráfica de pastel
+    var datos = {
+      labels: ["Retiros", "Depositos"],
+      datasets: [
+        {
+          data: [obtenerCantidadRetiros() ,obtenerCantidadDepositos()],
+          backgroundColor: ["#0D6EFD", "#DC3545"],
+          hoverBackgroundColor: ["#0D47A1", "#C62828"]      
+        }
+      ]
+    };
+
+    var opciones = {
+      responsive: true,
+      maintainAspectRatio: false
+    };
+
+    var grafica = new Chart(document.getElementById("acquisitions"), {
+      type: 'pie',
+      data: datos,
+      options: opciones
+    });
+}
+
 
